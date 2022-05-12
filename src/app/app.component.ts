207b5f6e0634f76b35e7829dcf9f33d6aed38f46
Siraj AHMADZAI <siraj.ahmadzai@amadeus.com>
2022-05-12 12:46:09 -0400

    angular app based on Educative.io's components lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'components';
+  posts = [
+    {
+      img: 'assets/image1.jpg',
+      author: 'John',
+      title: 'Title #1'
+    },
+    {
+      img: 'assets/image2.jpg',
+      author: 'Mike',
+      title: 'Title #2'
+    },
+    {
+      img: 'assets/image3.jpg',
+      author: 'Jane',
+      title: 'Title #3'
+    }
+  ]
 }
